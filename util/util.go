da975cf947150dc3b211f82241a872c5a77b6f00
Paul Hanyzewski <hanyzewski@gmail.com>
2022-12-27 15:43:37 -0700

    feat: create package utilities to share between packages

---
+++ util/util.go
@@ -0,0 +1,38 @@
+package util
+
+import (
+	"os"
+	"strconv"
+)
+
+// ValueOr returns a de-referenced string pointer, an environment variable, or a fallback
+func ValueOr(ptr *string, varName, defaultVal string) string {
+	if ptr != nil && *ptr != "" {
+		return *ptr
+	}
+
+	return GetEnv(varName, defaultVal)
+}
+
+// BooleanValueOr returns a de-referenced boolean pointer, an environment variable, or a fallback
+func BooleanValueOr(ptr *bool, varName, defaultVal string) bool {
+	if ptr != nil {
+		return *ptr
+	}
+
+	valStr := GetEnv(varName, defaultVal)
+	val, err := strconv.ParseBool(valStr)
+	if err != nil {
+		return false
+	}
+	return val
+}
+
+// GetEnv looks up an environment with a fallback default
+func GetEnv(varName, defaultVal string) string {
+	if value, isPresent := os.LookupEnv(varName); isPresent {
+		return value
+	}
+
+	return defaultVal
+}
