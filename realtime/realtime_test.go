068d9178e5a88813af9538b2808202412a6fec1e
Paul Hanyzewski <hanyzewski@gmail.com>
2023-04-30 10:39:41 -0600

    fixup! feat(realtime): first pass at realtime refactored into its own package

--- realtime/realtime_test.go
+++ realtime/realtime_test.go
@@ -11,25 +11,23 @@ import (
 	"testing"
 
 	"github.com/imup-io/client/realtime"
-	"github.com/imup-io/client/speedtesting"
 	"github.com/matryer/is"
 )
 
 func TestRealTime(t *testing.T) {
 	cases := []struct {
-		Name     string
-		ApiKey   string
-		Email    string
-		HostID   string
-		Realtime string
-		Status   string
-		Version  string
-		RetCode  int
+		Name    string
+		ApiKey  string
+		Email   string
+		HostID  string
+		Status  string
+		Version string
+		RetCode int
 	}{
-		{Name: "org", ApiKey: "1234", Email: "org-test@example.com", HostID: "org-based-host", Realtime: "true", Status: "running", Version: "unknown", RetCode: http.StatusNoContent},
-		// {Name: "org", ApiKey: "1234", Email: "org-test@example.com", HostID: "org-based-host", Realtime: "false", Status: "running", Version: "1.0.0", RetCode: http.StatusOK},
-		{Name: "user", ApiKey: "", Email: "test@example.com", HostID: "email-based-host", Realtime: "true", Version: "unknown", RetCode: http.StatusNoContent},
-		// {Name: "user", ApiKey: "", Email: "test@example.com", HostID: "email-based-host", Realtime: "false", Version: "unknown", RetCode: http.StatusNoContent},
+		{Name: "org", ApiKey: "1234", Email: "org-test@example.com", HostID: "org-based-host", Status: "running", Version: "1.0.0", RetCode: http.StatusNoContent},
+		{Name: "org", ApiKey: "1234", Email: "org-test@example.com", HostID: "org-based-host", Status: "running", Version: "1.0.0", RetCode: http.StatusOK},
+		{Name: "user", ApiKey: "", Email: "test@example.com", HostID: "email-based-host", Version: "unknown", RetCode: http.StatusNoContent},
+		{Name: "user", ApiKey: "", Email: "test@example.com", HostID: "email-based-host", Version: "unknown", RetCode: http.StatusNoContent},
 	}
 
 	for _, c := range cases {
@@ -37,14 +35,12 @@ func TestRealTime(t *testing.T) {
 		os.Setenv("API_KEY", c.ApiKey)
 		os.Setenv("EMAIL", c.Email)
 		os.Setenv("HOST_ID", c.HostID)
-		os.Setenv("REALTIME", c.Realtime)
 
 		t.Run(c.Name, testClientHealthy())
 		t.Run(c.Name, testShouldRunSpeedTest())
 		t.Run(c.Name, testShouldRunSpeedTestErrors())
 		t.Run(c.Name, testPostSpeedTestResult())
 		t.Run(c.Name, testRealTimeAuthorized(c.ApiKey, c.Email, c.Status))
-		t.Run(c.Name, testRemoteConfig(c.RetCode))
 		t.Run(c.Name, testRemoteUpdate(c.Name, c.Version))
 	}
 }
@@ -132,8 +128,7 @@ func testPostSpeedTestResult() func(t *testing.T) {
 		// os.Setenv("IMUP_ADDRESS_SPEEDTEST", testURL.String())
 		// os.Setenv("IMUP_SPEED_TEST_RESULTS_ADDRESS", testURL.String())
 
-		tr := &speedtesting.SpeedTestData{UploadMbps: 1.0, DownloadMbps: 1.0}
-		err := realtime.PostSpeedTestRealtimeResults(context.Background(), "complete", testURL.String(), tr.UploadMbps, tr.DownloadMbps)
+		err := realtime.PostSpeedTestRealtimeResults(context.Background(), "complete", testURL.String(), 1.0, 1.0)
 		is.NoErr(err)
 	}
 }
@@ -160,30 +155,6 @@ func testRealTimeAuthorized(apiKey, email, status string) func(t *testing.T) {
 		is.NoErr(err)
 	}
 }
-
-// TODO: right now this test is not configured with a proper api server response
-// func testRemoteConfig(retcode int) func(t *testing.T) {
-// 	return func(t *testing.T) {
-// 		is := is.New(t)
-
-// 		s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 			w.WriteHeader(http.StatusNoContent)
-// 		}))
-// 		defer s.Close()
-// 		testURL, _ := url.Parse(s.URL)
-
-// 		// TODO: include a test from run_test that sets this env var
-// 		// os.Setenv("IMUP_REALTIME_CONFIG", testURL.String())
-// 		cfg, err := realtime.NewConfig()
-// 		is.NoErr(err)
-
-// 		cfg.DisableRealtime()
-
-// 		c, err := realtime.RemoteConfigReload(context.Background(), testURL.String())
-// 		is.True(c.Realtime())
-// 		is.NoErr(err)
-// 	}
-// }
 
 func testRemoteUpdate(name, version string) func(t *testing.T) {
 	return func(t *testing.T) {
@@ -222,7 +193,6 @@ func testRemoteUpdate(name, version string) func(t *testing.T) {
 
 		// TODO: include a test from run_test that sets this env var
 		// os.Setenv("IMUP_REALTIME_CONFIG", testURL.String())
-
 		config, err := realtime.NewConfig()
 		is.NoErr(err)
 
@@ -234,8 +204,7 @@ func testRemoteUpdate(name, version string) func(t *testing.T) {
 			is.NoErr(err)
 			is.True(cfg.Realtime())
 		} else {
-			is.NoErr(err)
-			is.Equal(cfg.Realtime(), false)
+			is.True(err != nil)
 		}
 	}
 }
