c8e7b81d5f426a2bbe91aad014651841a46e400e
Paul Hanyzewski <hanyzewski@gmail.com>
2023-05-05 15:16:03 -0600

    feat: update ping to be more modular

--- connectivity/ping_test.go
+++ connectivity/ping_test.go
@@ -1,158 +1,158 @@
-package main
+package connectivity_test
 
 import (
 	"context"
 	"fmt"
-	"net/url"
 	"os"
-	"sync"
 	"testing"
 	"time"
 
+	"github.com/imup-io/client/connectivity"
 	"github.com/matryer/is"
 )
 
 func TestPing(t *testing.T) {
 	cases := []struct {
-		Name   string
-		ApiKey string
-		Email  string
-		HostID string
-		Ping   string
+		Name              string
+		Connected         bool
+		ConnectedInternal bool
+		Downtime          bool
+		ExternalPingAddrs []string
+		Opts              connectivity.PingOptions
 	}{
-		{Name: "org", ApiKey: "1234", Email: "org-test@example.com", HostID: "org-based-host", Ping: "true"},
-		{Name: "user", ApiKey: "", Email: "test@example.com", HostID: "email-based-host", Ping: "true"},
+		{
+			Name:              "connectivity-external-ping",
+			ExternalPingAddrs: []string{"8.8.8.8", "8.0.0.8"},
+			Connected:         true,
+			ConnectedInternal: true,
+			Downtime:          false,
+			Opts: connectivity.PingOptions{
+				AddressInternal: "",
+				AvoidAddrs:      map[string]bool{},
+				Count:           2,
+				Debug:           false,
+				Delay:           time.Duration(100) * time.Millisecond,
+				PingInterval:    time.Duration(1) * time.Second,
+				Timeout:         time.Duration(1) * time.Second,
+			},
+		},
+		{
+			Name:              "connectivity-no-internal-ping",
+			ExternalPingAddrs: []string{"240.0.0.0"},
+			Connected:         false,
+			ConnectedInternal: false,
+			Downtime:          false,
+			Opts: connectivity.PingOptions{
+				AddressInternal: "240.0.0.0",
+				AvoidAddrs:      map[string]bool{},
+				Count:           2,
+				Debug:           false,
+				Delay:           time.Duration(100) * time.Millisecond,
+				PingInterval:    time.Duration(1) * time.Second,
+				Timeout:         time.Duration(1) * time.Second,
+			},
+		},
+		{
+			Name:              "no-connectivity-internal-ping",
+			ExternalPingAddrs: []string{"240.0.0.0"},
+			Connected:         false,
+			ConnectedInternal: true,
+			Downtime:          true,
+			Opts: connectivity.PingOptions{
+				AddressInternal: "127.0.0.1",
+				AvoidAddrs:      map[string]bool{},
+				Count:           2,
+				Debug:           false,
+				Delay:           time.Duration(100) * time.Millisecond,
+				PingInterval:    time.Duration(1) * time.Second,
+				Timeout:         time.Duration(1) * time.Second,
+			},
+		},
+		{
+			Name:              "no-connectivity-no-ping",
+			ExternalPingAddrs: []string{"240.0.0.0"},
+			Connected:         false,
+			ConnectedInternal: false,
+			Downtime:          false,
+			Opts: connectivity.PingOptions{
+				AddressInternal: "",
+				AvoidAddrs:      map[string]bool{},
+				Count:           2,
+				Debug:           false,
+				Delay:           time.Duration(100) * time.Millisecond,
+				PingInterval:    time.Duration(1) * time.Second,
+				Timeout:         time.Duration(1) * time.Second,
+			},
+		},
 	}
 
 	for _, c := range cases {
 		os.Clearenv()
-		os.Setenv("API_KEY", c.ApiKey)
-		os.Setenv("EMAIL", c.Email)
-		os.Setenv("HOST_ID", c.HostID)
-		os.Setenv("PING_ENABLED", c.Ping)
-		os.Setenv("PING_INTERVAL", "1")
-		os.Setenv("PING_REQUESTS", "2")
-		os.Setenv("PING_DELAY", "100")
-		os.Setenv("PING_ADDRESS_INTERNAL", "127.0.0.1")
-
-		t.Run(fmt.Sprintf("test testCollectPingData for %s", c.Name), testCollectPingData())
-		t.Run(fmt.Sprintf("test testSendPingData for %s", c.Name), testSendPingData())
-		t.Run(fmt.Sprintf("test testPing for %s", c.Name), testPing())
-		t.Run(fmt.Sprintf("test testNoPingExternal for %s", c.Name), testNoPingExternal())
-		t.Run(fmt.Sprintf("test testNoPing for %s", c.Name), testNoPinger())
+		// TODO: include a test from run_test that sets this env var
+		// os.Setenv("API_KEY", c.ApiKey)
+		// os.Setenv("EMAIL", c.Email)
+		// os.Setenv("HOST_ID", c.HostID)
+		// os.Setenv("PING_ENABLED", c.Ping)
+		// os.Setenv("PING_INTERVAL", "1")
+		// os.Setenv("PING_REQUESTS", "2")
+		// os.Setenv("PING_DELAY", "100")
+		// os.Setenv("PING_ADDRESS_INTERNAL", "127.0.0.1")
+		// os.Setenv("PING_ADDRESS", testURL)
+		t.Run(fmt.Sprintf("test testCollectPingData for %s", c.Name), testCollectPingData(c.ExternalPingAddrs, c.Connected, c.ConnectedInternal, c.Downtime, c.Opts))
 	}
 }
 
-func testCollectPingData() func(t *testing.T) {
+func testCollectPingData(externalPingAddrs []string, connected, connectedInternal, downtime bool, opts connectivity.PingOptions) func(t *testing.T) {
 	return func(t *testing.T) {
 		is := is.New(t)
 
-		testURL := "8.8.8.8"
+		ping := connectivity.NewPingCollector(opts)
+		data := ping.Collect(context.Background(), externalPingAddrs)
+		is.True(len(data) >= 1)
+		_, dt := ping.DetectDowntime(data)
+		if downtime {
+			is.Equal(dt, 1)
+		} else {
+			is.Equal(dt, 0)
+		}
 
-		os.Setenv("PING_ADDRESS", testURL)
-		imup := newApp()
+		for _, stats := range data {
+			if stats.EndpointType == "external" {
+				is.Equal(connected, stats.Success)
+			}
+			if stats.EndpointType == "internal" {
+				is.Equal(connectedInternal, stats.SuccessInternal)
+			}
 
-		ping := imup.newPingStats()
-		data := ping.Collect(context.Background(), []string{testURL, "8.4.4.8"})
-		is.True(len(data) == 1)
-		for _, v := range data {
-			is.True(v.Success)
-			is.Equal(v.PacketsSent, imup.cfg.PingRequestsCount())
-			is.Equal(v.PacketsRecv, imup.cfg.PingRequestsCount())
+			if stats.Success {
+				is.Equal(stats.PacketsSent, opts.Count)
+				is.Equal(stats.PacketsRecv, opts.Count)
+				is.Equal(stats.SuccessInternal, true)
+			}
 		}
 	}
 }
 
-func testSendPingData() func(t *testing.T) {
-	return func(t *testing.T) {
-		s := defaultApiServer()
-		defer s.Close()
-		testURL, _ := url.Parse(s.URL)
+// TODO: move to imup_test
+// func testSendPingData() func(t *testing.T) {
+// 	return func(t *testing.T) {
+// 		s := defaultApiServer()
+// 		defer s.Close()
+// 		testURL, _ := url.Parse(s.URL)
 
-		os.Setenv("IMUP_ADDRESS", testURL.String())
-		imup := newApp()
+// 		os.Setenv("IMUP_ADDRESS", testURL.String())
+// 		imup := newApp()
 
-		data := imupData{
-			Downtime:      0,
-			StatusChanged: false,
-			Email:         imup.cfg.EmailAddress(),
-			ID:            imup.cfg.HostID(),
-			Key:           imup.cfg.APIKey(),
-			GroupID:       imup.cfg.GroupID(),
-			IMUPData:      []pingStats{},
-		}
+// 		data := imupData{
+// 			Downtime:      0,
+// 			StatusChanged: false,
+// 			Email:         imup.cfg.EmailAddress(),
+// 			ID:            imup.cfg.HostID(),
+// 			Key:           imup.cfg.APIKey(),
+// 			GroupID:       imup.cfg.GroupID(),
+// 			IMUPData:      []pingStats{},
+// 		}
 
-		sendImupData(context.Background(), sendDataJob{imup.cfg.PostConnectionData(), data})
-	}
-}
-
-func testPing() func(t *testing.T) {
-	return func(t *testing.T) {
-		is := is.New(t)
-
-		imup := newApp()
-
-		ps := imup.newPingStats()
-		pt, ok := ps.(*pingTest)
-		is.True(ok)
-		pinger, err := pt.setupExternalPinger(context.Background(), []string{"127.0.0.1"}, nil)
-		is.NoErr(err)
-
-		pinger.Timeout = time.Duration(imup.cfg.PingIntervalSeconds()) * time.Second
-		pinger.Interval = time.Duration(imup.cfg.PingDelayMilli()) * time.Millisecond
-		pinger.Count = imup.cfg.PingRequestsCount()
-		// successful run returns non nil error
-		stats, _ := pt.run(context.Background(), pinger)
-
-		is.True(stats.PacketsSent > 0)
-		is.True(stats.PacketsRecv > 0)
-	}
-}
-
-func testNoPingExternal() func(t *testing.T) {
-	return func(t *testing.T) {
-		is := is.New(t)
-		imup := newApp()
-
-		// https://superuser.com/questions/698244/ip-address-that-is-the-equivalent-of-dev-null
-		wg := sync.WaitGroup{}
-		wg.Add(1)
-		data := []pingStats{}
-		ps := imup.newPingStats()
-
-		go func() {
-			defer wg.Done()
-			data = ps.Collect(context.Background(), []string{"240.0.0.0"})
-		}()
-
-		wg.Wait()
-
-		for _, v := range data {
-			fmt.Println(v)
-			is.True(v.SuccessInternal)
-		}
-
-		changed, dt := ps.DetectDowntime(data)
-		is.True(changed == false)
-		is.Equal(dt, 0)
-
-	}
-}
-
-func testNoPinger() func(t *testing.T) {
-	return func(t *testing.T) {
-		is := is.New(t)
-
-		// https://superuser.com/questions/698244/ip-address-that-is-the-equivalent-of-dev-null
-		os.Setenv("IMUP_ADDRESS", "240.0.0.0")
-		imup := newApp()
-
-		ps := imup.newPingStats()
-		pt, ok := ps.(*pingTest)
-		is.True(ok)
-
-		pinger, err := pt.setupExternalPinger(context.Background(), []string{"240.0.0.0"}, nil)
-		is.True(err != nil)
-		is.True(pinger == nil)
-	}
-}
+// 		sendImupData(context.Background(), sendDataJob{imup.APIPostConnectionData, data})
+// 	}
+// }
