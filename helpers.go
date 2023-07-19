5e33809e639e00fe29682b153a0eae76a1580f73
Paul Hanyzewski <hanyzewski@gmail.com>
2023-07-19 16:03:04 -0600

    feat: use poisson instead of exponential distribution to prevent too frequent speed testing

--- helpers.go
+++ helpers.go
@@ -1,22 +1,28 @@
 package main
 
 import (
-	"math/rand"
 	"time"
 
+	xrand "golang.org/x/exp/rand"
 	log "golang.org/x/exp/slog"
+	"gonum.org/v1/gonum/stat/distuv"
 )
 
-// https://github.com/m-lab/ndt-server/blob/master/spec/ndt7-protocol.md#requirements-for-non-interactive-clients
-func sleepTime() time.Duration {
-	t := rand.ExpFloat64() * 21600
-	// currently set to 12960 for every 3.6 hours instead of 36 minutes
-	// to hack around a random seed issue
-	if t < 12960 {
-		t = 12960
-	} else if t > 54000 {
-		t = 54000
+// timePeriodSeconds at 21600 is six hours
+const timePeriodSeconds = 21600
+
+// speedTestInterval takes advantage of a poisson distribution
+// to generate pseudo random speed tests
+// this distribution helps guarantee a consistent number of speed tests
+// with a smaller chance of frequent speed tests consuming large amounts of data
+// or saturating a network
+func speedTestInterval() time.Duration {
+	poisson := distuv.Poisson{
+		Lambda: timePeriodSeconds,
+		Src:    xrand.NewSource(uint64(time.Now().UnixNano())),
 	}
+
+	t := poisson.Rand()
 
 	return time.Duration(t * float64(time.Second))
 }
