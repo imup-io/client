807fb0bed9945dbdaa188684626d325fe7f87038
Paul Hanyzewski <hanyzewski@gmail.com>
2023-07-21 12:10:59 -0600

    fix: use default source, create distribution based on minutes not seconds to create more variability in test frequency

--- helpers.go
+++ helpers.go
@@ -3,13 +3,14 @@ package main
 import (
 	"time"
 
-	xrand "golang.org/x/exp/rand"
+	// xrand "golang.org/x/exp/rand"
 	log "golang.org/x/exp/slog"
 	"gonum.org/v1/gonum/stat/distuv"
 )
 
-// timePeriodSeconds at 21600 is six hours
-const timePeriodSeconds = 21600
+// timePeriodMinutes is the desired interval between tests
+// the default (fixed) configuration is one tests every six hours
+const timePeriodMinutes = 6 * 60
 
 // speedTestInterval takes advantage of a poisson distribution
 // to generate pseudo random speed tests
@@ -17,14 +18,11 @@ const timePeriodSeconds = 21600
 // with a smaller chance of frequent speed tests consuming large amounts of data
 // or saturating a network
 func speedTestInterval() time.Duration {
-	poisson := distuv.Poisson{
-		Lambda: timePeriodSeconds,
-		Src:    xrand.NewSource(uint64(time.Now().UnixNano())),
-	}
+	t := distuv.Poisson{
+		Lambda: timePeriodMinutes,
+	}.Rand()
 
-	t := poisson.Rand()
-
-	return time.Duration(t * float64(time.Second))
+	return time.Duration(t * float64(time.Minute))
 }
 
 func drain(c chan sendDataJob) {
